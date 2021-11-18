2f7beb7ec2d066e645430c38834e97e95099d501
pavel <natitshow@gmail.com>
2021-11-18 23:52:24 +0300

    added customLogFilter().withCustomTemplates(

--- src/test/java/ShopTests.java
+++ src/test/java/ShopTests.java
@@ -4,6 +4,7 @@ import io.restassured.http.Cookies;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.*;
 
+import static filters.CustomLogFilter.customLogFilter;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
@@ -21,7 +22,7 @@ public class ShopTests {
         step("Get cookie by api and set it to browser", () -> {
             String body = String.format("//json");
             cookies = given()
-                    .filter(new AllureRestAssured())
+                    .filter(customLogFilter().withCustomTemplates())
                     .contentType("application/x-www-form-urlencoded; charset=UTF-8")
                     .formParam("Email", "qaguru@qa.guru")
                     .formParam("Password", "qaguru@qa.guru1")
@@ -34,7 +35,7 @@ public class ShopTests {
         });
         step("Add new product to basket", () -> {
             given()
-                    .filter(new AllureRestAssured())
+                    .filter(customLogFilter().withCustomTemplates())
                     .when()
                     .cookie(cookies.toString())
                     .post("http://demowebshop.tricentis.com/addproducttocart/catalog/31/1/1")
@@ -46,7 +47,7 @@ public class ShopTests {
 
         step("Add similar product to basket", () -> {
             given()
-                    .filter(new AllureRestAssured())
+                    .filter(customLogFilter().withCustomTemplates())
                     .when()
                     .cookie(cookies.toString())
                     .post("http://demowebshop.tricentis.com/addproducttocart/catalog/31/1/1")
@@ -58,7 +59,7 @@ public class ShopTests {
 
         step("Check cart", () -> {
             Response response = given()
-                    .filter(new AllureRestAssured())
+                    .filter(customLogFilter().withCustomTemplates())
                     .when()
                     .cookie(cookies.toString())
                     .get("http://demowebshop.tricentis.com/cart")
